d56a8c08bc67a8dc3d0ef9b456d26f41021fac1c
BoyThanakorn <thanakornjato@gmail.com>
2020-06-25 15:21:10 +0700

    eloquent params limit fix

--- src/Traits/BuilderCaching.php
+++ src/Traits/BuilderCaching.php
@@ -1,12 +1,53 @@
-<?php namespace GeneaLabs\LaravelModelCaching\Traits;
+<?php
+
+namespace GeneaLabs\LaravelModelCaching\Traits;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Database\SqlServerConnection;
 
 trait BuilderCaching
 {
-    public function all($columns = ['*']) : Collection
+    /**
+     * The maximum number of parameters per query.
+     *
+     * The values are lower than the actual limits
+     * to have a margin for polymorphic relationships
+     * and other query parameters.
+     *
+     * @var array
+     */
+    protected $parameterLimits = [
+        MySqlConnection::class => 65000,
+        SQLiteConnection::class => 900,
+        SqlServerConnection::class => 2000,
+    ];
+
+    /**
+     * Eager load the relationships for the models.
+     *
+     * @param array $models
+     * @return array
+     */
+    public function eagerLoadRelations(array $models)
     {
-        if (! $this->isCachable()) {
+        foreach ($this->parameterLimits as $class => $limit) {
+            if ($this->query->getConnection() instanceof $class && count($models) > $limit) {
+                foreach (array_chunk($models, $limit) as $chunk) {
+                    $this->eagerLoadRelations($chunk);
+                }
+
+                return $models;
+            }
+        }
+
+        return parent::eagerLoadRelations($models);
+    }
+
+    public function all($columns = ['*']): Collection
+    {
+        if (!$this->isCachable()) {
             $this->model->disableModelCaching();
         }
 
